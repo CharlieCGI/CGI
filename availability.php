52a44cd3953dbc4aee000bfaf65c9bb5093a0009
CharlieCGI <charlie.treloar@cgi.com>
2026-06-01 09:50:44 +0930

    Update availability.php

--- availability.php
+++ availability.php
@@ -1,1 +1,16 @@
 
+<?php
+require __DIR__ . '/includes/functions.php';
+
+header('Content-Type: application/json; charset=utf-8');
+
+$date = $_GET['date'] ?? '';
+$date = normalize_date($date);
+
+if ($date === '') {
+    echo json_encode(['ok' => false, 'slots' => [], 'error' => 'Invalid date']);
+    exit;
+}
+
+$slots = get_available_slots($date);
+echo json_encode(['ok' => true, 'slots' => $slots]);
