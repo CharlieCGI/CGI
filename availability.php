d064e91afe351e646066ea8527e1026dc5e0771b
CharlieCGI <charlie.treloar@cgi.com>
2026-06-01 09:39:42 +0930

    Add files via upload

---
+++ availability.php
@@ -0,0 +1,15 @@
+<?php
+require __DIR__ . '/includes/functions.php';
+
+header('Content-Type: application/json; charset=utf-8');
+
+$date = $_GET['date'] ?? '';
+$date = normalize_date($date);
+
+if ($date === '') {
+    echo json_encode(['ok' => false, 'slots' => [], 'error' => 'Invalid date']);
+    exit;
+}
+
+$slots = get_available_slots($date);
+echo json_encode(['ok' => true, 'slots' => $slots]);
